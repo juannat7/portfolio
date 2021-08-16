dfdcd12d8a54520def9dc0785368b53538d2efa2
Juan Nathaniel <nathanieljuan@gmail.com>
2021-08-16 18:56:29 +0800

    Updates

--- precache-manifest.c9aaa558c5e48fb649889c2d3ab39d4f.js
+++ precache-manifest.c9aaa558c5e48fb649889c2d3ab39d4f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cfe427c4ebb8dec8a8a9bb3b7e60755f",
+    "revision": "afe0e0b080a811fa873e8f2edd57a2ce",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "b4f03a2a303d180e4cd4",
+    "revision": "5ecc4cd079db8f7f9fa8",
     "url": "/portfolio/static/css/main.5facb584.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.af0baecc.chunk.js"
   },
   {
-    "revision": "b4f03a2a303d180e4cd4",
-    "url": "/portfolio/static/js/main.319a83ed.chunk.js"
+    "revision": "5ecc4cd079db8f7f9fa8",
+    "url": "/portfolio/static/js/main.fd2a3178.chunk.js"
   },
   {
     "revision": "76aa8cbd67b5c937ec2c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8120a68cb8ca5c8f337451b2acc5a685.js"
+  "/portfolio/precache-manifest.c9aaa558c5e48fb649889c2d3ab39d4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
